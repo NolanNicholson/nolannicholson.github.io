96f69593cd93c442eb640cb810d372c9d7a83d55
Nolan Nicholson <nolananicholson@gmail.com>
2019-11-10 12:15:56 -0800

    add clock ticks

--- scripts/clock.js
+++ scripts/clock.js
@@ -27,6 +27,7 @@ set_hand_css(second_hand, 45, 10);
 var number_container_node, number_node;
 var angle;
 var num_x, num_y;
+var r_numbers = 80;
 for (var i = 1; i <= 12; i++) {
     number_container_node = document.createElement("div");
     number_container_node.className = "clock-number-container";
@@ -38,12 +39,29 @@ for (var i = 1; i <= 12; i++) {
     number_container_node.appendChild(number_node);
 
     angle = ((i - 3) / 12) * Math.PI * 2;
-    r_numbers = 80;
-    num_x = parseInt(50 + (0.5 * Math.cos(angle) * r_numbers));
-    num_y = parseInt(50 + (0.5 * Math.sin(angle) * r_numbers));
+    num_x = 50 + (0.5 * Math.cos(angle) * r_numbers);
+    num_y = 50 + (0.5 * Math.sin(angle) * r_numbers);
 
     number_container_node.style.left = "" + num_x + "%";
     number_container_node.style.top = "" + num_y + "%";
+}
+
+//create clock ticks
+var tick_node;
+var r_ticks = 95;
+for (var i = 0; i < 60; i++) {
+    tick_node = document.createElement("div");
+    tick_node.className = "clock-tick";
+    clock_face.appendChild(tick_node);
+
+    angle = i / 60 * Math.PI * 2;
+    num_x = 50 + (0.5 * Math.cos(angle) * r_ticks);
+    num_y = 50 + (0.5 * Math.sin(angle) * r_ticks);
+
+    tick_node.style.left = "" + num_x + "%";
+    tick_node.style.top = "" + num_y + "%";
+    tick_node.style.transformOrigin = "50% 0%";
+    tick_node.style.transform = "translate(-50%, 0%) rotate(" + (i * 6 + 90) + "deg)";
 }
 
 function update_clock() {
