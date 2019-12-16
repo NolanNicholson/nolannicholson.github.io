4ffad1c9c1da6b7c477140b064233590e5e6242e
Nolan Nicholson <nolananicholson@gmail.com>
2019-12-16 10:27:53 -0800

    Revert "gravity now also framerate-independent"
    This reverts commit 6c4ee2e10244078f3ca5b127796eae6d219b2fa2.

--- scripts/latency_demo.js
+++ scripts/latency_demo.js
@@ -233,7 +233,7 @@ class Bird {
     }
 
     update(fps) {
-        this.vy += (BIRD_GRAVITY * 60 / fps);
+        this.vy += BIRD_GRAVITY;
         if (this.vy > BIRD_MAX_VY) this.vy = BIRD_MAX_VY;
 
         this.y_pct += (this.vy * 60 / fps);
